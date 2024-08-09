7823c3ccb225563350b4bc8d33a6b098ac34fd80
Adhinarayanan P <adhinarayanan.p@pptssolutions.onmicrosoft.com>
2024-08-09 10:50:25 +0530

    Navbar Update

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function () {
+    const navbar = document.getElementById('navbarExample');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
